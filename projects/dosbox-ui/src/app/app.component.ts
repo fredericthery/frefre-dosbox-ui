46faeaf5103a245e77b187225cd2e5e00418d38f
Frederic THERY <46963335+fredericthery@users.noreply.github.com>
2024-12-08 22:25:37 +0100

    Add sidenav and header (#5)

--- projects/dosbox-ui/src/app/app.component.ts
+++ projects/dosbox-ui/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainSidenavComponent } from "./common/main-sidenav/main-sidenav.component";
+import { MainHeaderComponent } from "./common/main-header/main-header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainSidenavComponent, MainHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
