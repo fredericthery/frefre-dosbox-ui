b29d151e8e555047b88b7652d77ddad295b565d3
Frederic THERY <46963335+fredericthery@users.noreply.github.com>
2024-12-09 21:05:39 +0100

    Add route home (#8)

--- projects/dosbox-ui/src/app/app.routes.ts
+++ projects/dosbox-ui/src/app/app.routes.ts
@@ -1,5 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeContentComponent } from './ui/home/home-content/home-content.component';
 
 export const routes: Routes = [
-
+  {path: 'home', component: HomeContentComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: '**', redirectTo: 'home'},
 ];
